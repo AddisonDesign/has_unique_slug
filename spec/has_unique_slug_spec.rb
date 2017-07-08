3851134a63877bc8fa8415a5e6c5c51e12c3c8b6
Chris Scharf <scharfie@gmail.com>
2017-07-08 08:44:31 -0400

    update rspec syntax

--- spec/has_unique_slug_spec.rb
+++ spec/has_unique_slug_spec.rb
@@ -71,14 +71,14 @@ describe HasUniqueSlug do
   
   it "creates a unique slug" do
     r = Standard.create! :title => "Sample Record"
-    r.slug.should == "sample-record"
+    expect(r.slug).to eq "sample-record"
   end
   
   it "should add incremental column if not unique" do
     Standard.create! :title => "Sample Record"
     2.upto 5 do |i|
       r = Standard.create! :title => "Sample Record"
-      r.slug.should == "sample-record-#{i}"
+      expect(r.slug).to eq "sample-record-#{i}"
     end
   end
 
@@ -86,17 +86,17 @@ describe HasUniqueSlug do
     r = Standard.create! :title => "Sample Record"
     r.slug = "another-slug"
     r.save
-    r.slug.should == "another-slug"
+    expect(r.slug).to eq "another-slug"
   end
 
   it "should be able to manually set a slug and ensure it is unique for new records" do
     r1 = Standard.create! :title => "Sample Record", slug: "another-slug"
     r1.reload
-    r1.slug.should == "another-slug"
+    expect(r1.slug).to eq "another-slug"
 
     r2 = Standard.create! :title => "Sample Record", slug: "another-slug"
     r2.reload
-    r2.slug.should == "another-slug-2"
+    expect(r2.slug).to eq "another-slug-2"
   end
 
   it "should be able to manually set a slug and ensure it is unique for existing records" do
@@ -104,62 +104,62 @@ describe HasUniqueSlug do
     r1.slug = "another-slug"
     r1.save
     r1.reload
-    r1.slug.should == "another-slug"
+    expect(r1.slug).to eq "another-slug"
 
     r2 = Standard.create! :title => "Sample Record"
     r2.slug = "another-slug"
     r2.save
     r2.reload
-    r2.slug.should == "another-slug-2"
+    expect(r2.slug).to eq "another-slug-2"
   end
   
   it "should not increment the slug if the duplicate is itself" do
     r = Standard.create! :title => "Sample Record"
     slug = r.slug
-    r.save.should be_true
-    r.slug.should == slug
+    expect(r.save).to be true
+    expect(r.slug).to eq slug
   end
   
   it "should update slugs for non-standard implementation" do
     r = Custom.create! :name => "Sample Record"
-    r.permalink.should == "sample-record"
+    expect(r.permalink).to eq "sample-record"
     2.upto 5 do |i|
       r = Custom.create! :name => "Sample Record"
-      r.permalink.should == "sample-record-#{i}"
+      expect(r.permalink).to eq "sample-record-#{i}"
     end
     r = Custom.last
     slug = r.permalink
-    r.save.should be_true
-    r.permalink.should == slug
+    expect(r.save).to be true
+    expect(r.permalink).to eq slug
   end
   
   it "should update slugs based on the block if a block is provided" do
     r = Custom2.create! :name => "Sample Record"
-    r.permalink.should == "zcvf-sample-record-zxvf"
+    expect(r.permalink).to eq "zcvf-sample-record-zxvf"
     2.upto 5 do |i|
       r = Custom2.create! :name => "Sample Record"
-      r.permalink.should == "zcvf-sample-record-zxvf-#{i}"
+      expect(r.permalink).to eq "zcvf-sample-record-zxvf-#{i}"
     end
     r = Custom2.last
     slug = r.permalink
-    r.save.should be_true
-    r.permalink.should == slug
+    expect(r.save).to be true
+    expect(r.permalink).to eq slug
   end
   
   it "should allow two slugs with the same value if scopes differ" do
     r = StandardWithScope.create! :title => "Sample Record", :some_scope => 1
-    r.slug.should == "sample-record"
+    expect(r.slug).to eq "sample-record"
     
     r = StandardWithScope.create! :title => "Sample Record", :some_scope => 2
-    r.slug.should == "sample-record"
+    expect(r.slug).to eq "sample-record"
   end
   
   it "should increment the slug if two records share the same scope" do
     r = StandardWithScope.create! :title => "Sample Record", :some_scope => 1
-    r.slug.should == "sample-record"
+    expect(r.slug).to eq "sample-record"
     
     r = StandardWithScope.create! :title => "Sample Record", :some_scope => 1
-    r.slug.should == "sample-record-2"
+    expect(r.slug).to eq "sample-record-2"
   end
 
   it "will not raise an exception if subject column is blank" do
@@ -171,7 +171,7 @@ describe HasUniqueSlug do
     car = Car.create(:title => "El Camino")
     truck = Truck.create(:title => "El Camino")
 
-    car.slug.should_not == truck.slug
+    expect(car.slug).not_to eq(truck.slug)
   end
 
   it "should allow scope as proc" do
@@ -179,6 +179,6 @@ describe HasUniqueSlug do
     a = DeletedScopeProc.create(:title => title, :deleted_at => Time.now)
     b = DeletedScopeProc.create(:title => title)
 
-    b.slug.should == a.slug
+    expect(b.slug).to eq a.slug
   end
 end
